16dafb9c1f58926797b9c2b3334f0e5b27fd8b85
mmullens <mmullens@hoonto.com>
2013-06-19 20:05:15 +0000

    added the angle_box_2d and angle_contains_ray_2d functions

--- pygeometry.py
+++ pygeometry.py
@@ -4,6 +4,8 @@ import cProfile
 from ctypes import *
 lib = cdll.LoadLibrary('./libgeometry.so')
 
+lib.angle_box_2d.restype = None
+lib.angle_contains_ray_2d.restype = c_int
 lib.sphere_distance1.restype = c_double
 lib.sphere_distance2.restype = c_double
 lib.sphere_distance3.restype = c_double
@@ -11,6 +13,21 @@ lib.polygon_contains_point_2d.restype = c_bool
 lib.polygon_contains_point_2d_2.restype = c_bool
 
 class Geometry():
+
+    def angle_box_2d ( dist, p1, p2, p3, p4, p5 ):
+        cp1 = (c_double * 2)(*p1)
+        cp2 = (c_double * 2)(*p2)
+        cp3 = (c_double * 2)(*p3)
+        cp4 = (c_double * 2)(*p4)
+        cp5 = (c_double * 2)(*p5)
+        lib.angle_box_2d(c_double(dist), cp1, cp2, cp3, cp4, cp5);
+
+    def angle_contains_ray_2d ( p1, p2, p3, p):
+        cp1 = (c_double * 2)(*p1)
+        cp2 = (c_double * 2)(*p2)
+        cp3 = (c_double * 2)(*p3)
+        cp = (c_double * 2)(*p)
+        return lib.angle_contains_ray_2d(cp1, cp2, cp3, cp);
 
     def sphere_distance1(self, lat1, lon1, lat2, lon2, r):
         return lib.sphere_distance1(c_double(lat1), c_double(lon1), c_double(lat2), c_double(lon2), c_double(r)) 
@@ -45,13 +62,8 @@ def test(numtests):
         g.sphere_distance1(39.7391500 * toradians, -104.9847000 * toradians, 34.0522300 * toradians, -118.2436800 * toradians, 6371)
         g.polygon_contains_point_2d([0,0,0,4,4,4,4,0], [1,1])
 
-        ####
-        # The distance methods, you can try different distance algorithms
-        # and see what if any effect there is on the test results:
-        #g.sphere_distance2(39.7391500 * toradians, -104.9847000 * toradians, 34.0522300 * toradians, -118.2436800 * toradians, 6371)
-        #g.sphere_distance3(39.7391500 * toradians, -104.9847000 * toradians, 34.0522300 * toradians, -118.2436800 * toradians, 6371)
-        ##
 
+#test(1);
 
 # 2000 calls to a sphere_distance1
 # and 2000 calls to polygon_contains_point_2d
